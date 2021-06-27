b78c75ac5ff579e4a8a031111f241c2bea1d25c3
kitsuka <hi.kitsuka@gmail.com>
2021-06-28 01:27:56 +0900

    Modify Navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Badge } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, IconButton, Badge } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import AccountCircle from '@material-ui/icons/AccountCircle';
 // import { FiLogOut } from "react-icons/fi";
 
 const useSytle = makeStyles((theme) => ({
-  bg: {
-    marginRight: theme.spacing(1),
+  toolbar: {
+    minHeight: '42px',
+    paddingLeft: '0px'
   },
   title: {
-    fontFamily: 'Oleo Script',
     flexGrow: 1,
+    fontFamily: 'Oleo Script',
+    textAlign: "left",
+    display: 'block',
+    marginLeft: theme.spacing(3)
+  },
+  sectionDesktop: {
+    display: 'flex',
   },
 }));
 
@@ -20,22 +29,34 @@ const Navbar = () => {
   const classes = useSytle();
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h5" className={classes.title}>
+      <Toolbar className={classes.toolbar}>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h5" noWrap>
           Fluent Task
         </Typography>
-        <Badge
-          className={classes.bg}
-          badgeContent={1}
-          color="secondary"
-        >
-          <NotificationsIcon />
-        </Badge>
-        <button className="signOut" onClick={signOut}>
-          {/* <FiLogOut /> */}
-        </button>
+        <div className={classes.sectionDesktop}>
+          <IconButton aria-label="show new notifications">
+            <Badge
+              badgeContent={1}
+              color="secondary"
+            >
+              <NotificationsIcon />
+            </Badge >
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="account of current user"
+            // aria-controls={menuId}
+            aria-haspopup="true"
+          // onClick={handleProfileMenuOpen}
+          >
+            <AccountCircle />
+          </IconButton>
+        </div>
       </Toolbar>
-    </AppBar>
+    </AppBar >
   );
 };
 
