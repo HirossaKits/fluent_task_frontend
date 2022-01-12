99c2a67caa03828f79a5c99ae5099a74a848908e
kitsuka <hi.kitsuka@gmail.com>
2022-01-12 18:58:13 +0900

    画像をドラッグできないように変更

--- src/components/CommonAvatar.tsx
+++ src/components/CommonAvatar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Avatar from '@mui/material/Avatar';
 import { ORG_USER } from '../features/types';
 
@@ -9,11 +10,19 @@ type Props = {
 const CommonAvatar = (props: Props) => {
   console.log(props.user);
 
+  const style = css`
+    user-drag: none;
+    -webkit-user-drag: none;
+    -moz-user-select: none;
+  `;
+
   return (
     <>
       {props.user ? (
         props.user.avatar_img ? (
-          <Avatar src={props.user.avatar_img} />
+          <Avatar>
+            <img css={style} src={props.user.avatar_img} alt='avatar' />
+          </Avatar>
         ) : (
           <Avatar>
             {props.user.last_name.slice(0, 1) +
