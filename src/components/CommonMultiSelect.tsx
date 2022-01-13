e4ba4b4e66881739349d47d758bd6147c202dc60
kits-create <hi.kitsuka@gmail.com>
2022-01-13 12:30:20 +0900

    MultiSelect修正完了

--- src/components/CommonMultiSelect.tsx
+++ src/components/CommonMultiSelect.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
 import Autocomplete, {
   AutocompleteChangeDetails,
   AutocompleteChangeReason,
-} from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import { TARGET } from '../features/types';
+} from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { TARGET } from "../features/types";
 
 type Option = {
   value: string;
@@ -38,52 +38,42 @@ export default function CommonMultiSelect(props: Props) {
     props.onChange(target);
   };
 
-  const test = (
-    event: React.SyntheticEvent<Element, Event>,
-    value: Option[]
-  ) => {
-    let target: TARGET = {
-      name: props.name,
-      value: value.map((option) => option.value),
-    };
-    props.onChange(target);
-  };
-
-  // attr
-  // https://codesandbox.io/s/material-demo-kznv3?file=/demo.js:866-881-999
+  const selectedValues = props.value.map((val) => val.value);
 
   return (
     <Autocomplete
       multiple
       options={props.options}
-      // value={props.value}
+      value={props.value}
       disableCloseOnSelect
       onChange={handleSelectedChange}
-      // onChange={(event, value) => test(event, value)}
       getOptionLabel={(option) => option.label}
-      renderOption={(option, state) => {
-        const selected = props.value.findIndex((val) => val);
+      renderOption={(props_ch, option, { selected }) => {
+        console.log("test", props);
+        console.log("test", option);
+        const isChecked = selectedValues.includes(option.value);
+
         return (
-          <li {...props}>
+          <li {...props_ch}>
             <Checkbox
-              icon={<CheckBoxOutlineBlankIcon fontSize='small' />}
-              checkedIcon={<CheckBoxIcon fontSize='small' />}
+              icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+              checkedIcon={<CheckBoxIcon fontSize="small" />}
               style={{ marginRight: 8 }}
-              checked={selected}
+              checked={isChecked}
             />
             {option.label}
           </li>
         );
       }}
-      style={{ width: props.width ?? '100%' }}
+      style={{ width: props.width ?? "100%" }}
       renderInput={(params) =>
         props.readOnly ? (
           <TextField
             {...params}
-            label={props.label ?? ''}
-            placeholder={props.placeholder ?? ''}
-            variant='standard'
-            margin='normal'
+            label={props.label ?? ""}
+            placeholder={props.placeholder ?? ""}
+            variant="standard"
+            margin="normal"
             InputLabelProps={{
               shrink: true,
             }}
@@ -91,10 +81,10 @@ export default function CommonMultiSelect(props: Props) {
         ) : (
           <TextField
             {...params}
-            label={props.label ?? ''}
-            placeholder={props.placeholder ?? ''}
-            variant='standard'
-            margin='normal'
+            label={props.label ?? ""}
+            placeholder={props.placeholder ?? ""}
+            variant="standard"
+            margin="normal"
           />
         )
       }
